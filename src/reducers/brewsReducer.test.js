d1d697b9f0cf15dda0305f9d995cf3d22bd3904f
ssheaks <sarah.sheaks@gmail.com>
2018-05-03 09:30:18 -0700

    set up brewReducer boilerplate

---
+++ src/reducers/brewsReducer.test.js
@@ -0,0 +1,33 @@
+import brewsReducer from './brewsReducer';
+import { fetchBrewsRequest, fetchBrewsSuccess, fetchBrewsError, addBrew,deleteBrewSuccess} from '../actions/brewsAct';
+
+describe('brewsReducer', () => {
+  it('should set the initial state when nothing is passed in', () => {
+
+  });
+
+  it('should return the current state on an unknown action', () => {
+
+  });
+
+  it('should handle the fetchBrewsRequest action', () => {
+
+  });
+
+  it('should handle the fetchBrewsSuccess action', () => {
+
+  });
+
+  it('should handle the fetchBrewsError action', () => {
+
+  });
+
+  it('should handle the addBrew action', () => {
+
+  });
+
+  it('should handle the deleteBrewSuccess action', () => {
+
+  });
+
+})
