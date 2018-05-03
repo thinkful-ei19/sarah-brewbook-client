b94366438f445a00915a63753cfe7fd2b53650f1
ssheaks <sarah.sheaks@gmail.com>
2018-05-03 09:20:39 -0700

    finish sync brewsAct tests

--- src/actions/brewsAct.test.js
+++ src/actions/brewsAct.test.js
@@ -21,3 +21,40 @@ describe('fetchBrewsSuccess', () => {
     });
   });
 });
+
+describe('fetchBrewsError', () => {
+  it('Should return the action', () => {
+    const error = 's@r@h';
+    const action = fetchBrewsError(error);
+    expect(action).toEqual({
+      type: FETCH_BREWS_ERROR,
+      error
+    });
+  });
+});
+
+describe('addBrew', () => {
+  it('Should return the action', () => {
+    const brew = {
+      'name': 'watermelon wheat',
+      'recipe': 'hops, yeast, grain',
+      'notes': 'good summer brew'
+    };
+    const action = addBrew(brew);
+    expect(action).toEqual({
+      type: ADD_BREW,
+      brew
+    });
+  });
+});
+
+describe('deleteBrewSuccess', () => {
+  it('Should return the action', () => {
+    const id = 123456789123456789;
+    const action = deleteBrewSuccess(id);
+    expect(action).toEqual({
+      type: DELETE_BREW_SUCCESS,
+      id
+    });
+  });
+});
